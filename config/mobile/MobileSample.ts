210a7bdbec9e76556c92d046ccf88b82e122d667
pangphanna <pangphanna@mcnc.co.kr>
2025-06-21 21:12:23 +0700

    Remove source files from GitHub, keep locally

--- config/mobile/MobileSample.ts
+++
@@ -1,8 +0,0 @@
-import core from "./../coreBridge";
-import { SampleInterface } from "../PlatformInterface";
-
-export class MobileSample implements SampleInterface {
-    sampleMethod(): void {
-        // core.sample();
-    }
-}
